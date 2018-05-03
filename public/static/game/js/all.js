60557b94e712f389d9b3906d595eb0186bb58c42
yijinchun@outlook.com <yijinchun@outlook.com>
2018-05-03 20:51:47 +0800

    添加 飞机大战

---
+++ public/static/game/js/all.js
@@ -0,0 +1,19 @@
+var father = new Object();
+
+//设置根目录的rem
+(function (doc, win) {
+  var docEl = doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      if (clientWidth >= 750) {
+        docEl.style.fontSize = '100px';
+      } else {
+        docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+      }
+    };
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
