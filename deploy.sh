9e6843c3941f51605c8d5772130fa3b0a5e31901
jaceyi <yijinchun@outlook.com>
2019-05-30 23:24:39 +0800

    调整结构

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 mkdir dist
 
+cp -rf index.html dist/index.html
+cp -rf style dist/style
 cp -rf error dist/error
-cp -rf home dist/home
 cp -rf static dist/static
-cp app.js dist/app.js
-cp package.json dist/package.json
 
-rsync -avzP --delete ./dist/* root@yijic.com:~/web/
+scp -r ./dist/* root@152.32.172.101:/data/web
 
 rm -rf dist
