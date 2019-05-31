bc98253171fce2612acff1be70c63508db621b72
jaceyi <yijinchun@outlook.com>
2019-05-31 10:51:09 +0800

    删除多余图片

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 mkdir dist
 
-cp -rf index.html dist/index.html
+cp -f index.html dist/index.html
+cp -f favicon.ico dist/favicon.ico
 cp -rf style dist/style
 cp -rf error dist/error
 cp -rf static dist/static
 
-scp -r ./dist/* root@152.32.172.101:/data/web
+scp -r ./dist/* root@yijic.com:/data/web
 
 rm -rf dist
