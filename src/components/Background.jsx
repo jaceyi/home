8971543b7e83aae8da833fbd6d5d66da49f5f10d
jaceyi <yijinchun@outlook.com>
2021-07-09 11:51:22 +0800

    fix: attachment fixed

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -5,16 +5,22 @@ const Background = ({ url, brightness = 1 }) => {
   return (
     <div
       css={css`
-          position: absolute;
-          left: 0;
-          top: 0;
-          width: 100%;
-          height: 100%;
-          background: url('${url}') no-repeat left center fixed;
-          background-size: cover;
-          filter: brightness(${brightness});
-          z-index: -1;
-        `}
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background: url(${url}) no-repeat center center;
+        background-size: cover;
+        filter: brightness(${brightness});
+        z-index: -1;
+
+        @media screen and (min-width: 500px) {
+          & {
+            background-attachment: fixed;
+          }
+        }
+      `}
     />
   );
 };
